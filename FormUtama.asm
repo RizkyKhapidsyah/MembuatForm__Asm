ec030a184f58448002428d3d307caf07aa868117
Rizky Khapidsyah <45317201+RizkyKhapidsyah@users.noreply.github.com>
2020-05-16 18:34:48 +0700

    Update FormUtama.asm

--- FormUtama.asm
+++ FormUtama.asm
@@ -6,37 +6,31 @@ include \masm32\include\windows.inc
 include \masm32\include\user32.inc
 include \masm32\include\kernel32.inc
 
-
 .Data?
-
 .Data
-
 .Code
 
 WndProc proc hWnd:DWORD, uMsg:DWORD, wParam:DWORD, lParam:DWORD
-	.IF uMsg==WM_DESTROY
+	.IF uMsg == WM_DESTROY
 		invoke PostQuitMessage,NULL
-	.ELSEIF uMsg==WM_COMMAND
-		mov eax,wParam
+	.ELSEIF uMsg == WM_COMMAND
+		mov eax, wParam
 		.IF lParam==0
-			; Process messages, else...
 			invoke DestroyWindow,hWnd
 		.ELSE
-			mov edx,wParam
-			shr edx,16
-			; Process messages here
+			mov edx, wParam
+			shr edx, 16
 		.ENDIF
 	.ELSE
-		invoke DefWindowProc,hWnd,uMsg,wParam,lParam
+		invoke DefWindowProc, hWnd, uMsg, wParam, lParam
 		ret
 	.ENDIF
-	xor	eax,eax
+	xor eax,eax
 	ret
 WndProc endp
 
 ButtonSimpanClick proc hWnd:DWORD, uMsg:DWORD, wParam:DWORD, lParam:DWORD
-     ; Your code here
-     xor eax, eax	; return false
+     xor eax, eax
      ret
 ButtonSimpanClick endp
 
